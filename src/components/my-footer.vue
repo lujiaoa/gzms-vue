c8879a64eab811bc82323d4d8a94578a1305652d
e <www.1181065872@qq.com>
2020-10-21 20:32:20 +0800

    完成了404，修改了foot组件

--- src/components/my-footer.vue
+++ src/components/my-footer.vue
@@ -1,28 +1,16 @@
 <template>
     <div id="my_footer">
         <mt-tabbar v-model="tabbar" fixed>
-            <mt-tab-item id="index">
-                首页
-                <!-- //199E34 -->
-                <!-- //707070 --> 
-                <img src="..\assets\footer-images\index_enabled.png" slot="icon" v-if="tabbar=='index'" >
-                <img src="..\assets\footer-images\index_disabled.png" slot="icon"  v-else alt="">
-            </mt-tab-item>
-            <mt-tab-item id="collect">
-                收藏
-                <img src="..\assets\footer-images\collect_enabled.png" slot="icon" v-if="tabbar=='collect'" alt="">
-                <img src="..\assets\footer-images\collect_disabled.png" slot="icon" v-else alt="">
-            </mt-tab-item>
-            <mt-tab-item id="message">
-                消息
-                <img src="..\assets\footer-images\message_enabled.png" slot="icon" v-if="tabbar=='message'" alt="">
-                <img src="..\assets\footer-images\message_disabled.png" slot="icon" v-else alt="">
+            <mt-tab-item id="index" >
+                  首页 
+                    <img src="..\assets\footer-images\index_enabled.png" slot="icon" v-if="tabbar=='index'" >
+                    <img src="..\assets\footer-images\index_disabled.png" slot="icon"  v-else alt="">  
             </mt-tab-item>
             <mt-tab-item id="me" class="MyFontStyle">
-                我的
+                    我的
                 <img src="..\assets\footer-images\me_enabled.png" slot="icon" v-if="tabbar=='me'" alt="">
                 <img src="..\assets\footer-images\me_disabled.png" slot="icon" v-else alt="">
-            </mt-tab-item>
+            </mt-tab-item>    
         </mt-tabbar>
     </div>
 </template>
@@ -33,16 +21,25 @@
     .mint-tabbar>.is-selected{
         color:#aa5 ;
     }
-    .mint-tabbar>.mint-tab-item{
-         font-size: ;
-    }
 </style>
 <script>
 export default {
     data(){
         return{
-            tabbar:'me'
+            tabbar:'index'
         }
-    }
+    },
+   watch:{
+       tabbar(value){
+           if(value=='me'){
+                this.$router.push({path:'/me'})
+                this.tabbar='me'
+           }else if(value=='index'){
+               this.tabbar='index'
+                this.$router.push({path:'/'})
+           }
+       }
+
+   }
 }
 </script>
